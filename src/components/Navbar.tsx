e3eab029ebda4e581787f1475195ae7b171a9a3e
Om Jejurkar <140536547+OmJejurkar@users.noreply.github.com>
2025-01-18 22:28:02 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center gap-3">
             <img 
-              src="./src/images/logo.jpg" 
+              src="../src/images/logo.jpg" 
               alt="Jayantak Logo" 
               className="h-12 w-12 object-contain"
             />
