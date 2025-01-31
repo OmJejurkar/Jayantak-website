9c38a5078da567202ea003907214597ca2044a8c
Om Jejurkar <140536547+OmJejurkar@users.noreply.github.com>
2025-01-31 13:16:01 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
       id="home"
       className="pt-16 bg-gradient-to-br from-black via-purple-900 to-black text-white"
     >
-      <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-32">
+      <div className="max-w-auto mx-auto px-4 sm:px-6 lg:px-8 py-32">
         <div className="grid md:grid-cols-2 gap-12 items-center">
           <div>
             <h1 className="text-5xl font-bold mb-6">
