3d7a7a2d145fbee6726a751ef23c2fb865ac757e
Om Jejurkar <jejurkarom24@gmail.com>
2025-03-28 14:31:35 +0530

    image updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Menu, X, } from 'lucide-react';
-import logoWBG from "../images/logoWBG.jpg";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -11,7 +10,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center gap-3">
             <img 
-              src={logoWBG} 
+              src="/images/logoWBG.jpg" 
               alt="Jayantak Logo" 
               className="h-12 w-12 object-contain"
             />
